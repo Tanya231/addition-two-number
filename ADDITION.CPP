08a4e28fc399af11819f3969eebf374bc987c040
Tanya Singh <53834227+Tanya231@users.noreply.github.com>
2019-08-31 14:11:56 +0530

    Add files via upload

---
+++ ADDITION.CPP
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+float a,b,c;
+printf("Enter any two numbers :");
+scanf("%f%f",&a,&b);
+c=a+b;
+printf("Addition =%f\n" , c);
+getch();
+}
